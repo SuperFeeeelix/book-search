6b23b4d60ccebeee34203fe407c6f67299012797
SuperFeeeelix <127154412+SuperFeeeelix@users.noreply.github.com>
2023-09-21 21:17:56 -0700

    added code

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const db = require('./config/connection');
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schema');
 
 const { authMiddleware } = require('./utils/auth');
 
